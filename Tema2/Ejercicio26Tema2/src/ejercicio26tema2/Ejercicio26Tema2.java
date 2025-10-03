432ae04fca6d57cc602f5fbeddb98bb0f258b4ed
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2025-10-03 16:31:06 +0200

    Ejercicio26-32

---
+++ Tema2/Ejercicio26Tema2/src/ejercicio26tema2/Ejercicio26Tema2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio26tema2;
+import java.util.Scanner;
+/**
+ *
+ * @author DAVID
+ */
+public class Ejercicio26Tema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Por favor, introduzca un numero de 4 cifras: ");
+        int numero = entrada.nextInt();
+        
+        int primeraCifra = numero / 1000;//Se divide para sacar la primera cifra
+        int segundaCifra = (numero / 100) % 10;//Se divide y salen dos cifras que a su vez se divide y se saca el resto y sale la segunda cifra 
+        int terceraCifra = (numero /10) % 10;//Se divide y salen tres cifras que a su vez se divide y se saca el resto y sale la tercera cifra
+        int cuartaCifra = numero % 10;//Se divide para sacar la cuarta cifra, pero se saca del resto
+        
+        System.out.println("La primera cifra es: " + primeraCifra);
+        System.out.println("La segunda cifra es: " + segundaCifra);
+        System.out.println("La tercera cifra es: " + terceraCifra);
+        System.out.println("La cuarta cifra es: " + cuartaCifra);
+    }
+    
+}
---
+++ Tema2/Ejercicio32Tema2/src/ejercicio32tema2/Ejercicio32Tema2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio32tema2;
+import java.util.Scanner;
+/**
+ *
+ * @author DAVID
+ */
+public class Ejercicio32Tema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Por favor, indique una cantidad de dinero: ");
+        int importe = entrada.nextInt();
+        int cantidadDinero = importe;//Lo pongo para que el importe que ponga el cliente sea la cantidad del dinero y se pueda hacer las operaciones y en System.out.println salga el otra vez el importe que puso el cliente
+        
+        int billeteCincuenta = cantidadDinero / 50;
+        cantidadDinero %= 50;//Esta puesto para que el resto del dinero que falta se vaya a la cantida del dinero total y se hace igual que en todas las demas operaciones
+        
+        int billeteVeinte = cantidadDinero / 20;
+        cantidadDinero %= 20;
+        
+        int billeteDiez = cantidadDinero / 10;
+        cantidadDinero %= 10;
+        
+        int billeteCinco = cantidadDinero / 5;
+        cantidadDinero %= 5;
+        
+        int monedasDos = cantidadDinero / 2;
+        cantidadDinero %= 2;
+        
+        int monedasUno = cantidadDinero;
+        
+        System.out.println("");//Lo pongo para que haya una separacion y se vea mejor el resultado
+        System.out.println(importe + " Euros se descomponen en " + billeteCincuenta + " billetes de 50, " + billeteVeinte + " billetes de 20, " + billeteDiez + " billete de 10, " + billeteCinco + " billete de 5, " + monedasDos + " monedas de 2 euros y " + monedasUno + " monedas de 1 euro");
+    }
+    
+}
