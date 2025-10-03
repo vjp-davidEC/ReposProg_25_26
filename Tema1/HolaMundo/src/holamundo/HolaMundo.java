6f52fb37e114e8fb076027036c37105b22261e63
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2025-10-03 16:29:24 +0200

    Hola mundo

---
+++ Tema1/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+/**
+ *Mi primer programa
+ * @author David
+ * @since 2025
+ */
+public class HolaMundo {
+
+    /**
+     * Método main
+     * @since 2025
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo!!!");
+    }
+    
+}
