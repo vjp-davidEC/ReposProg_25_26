e00b18f0ec0f0bc66cb0b900b51ec58a620eb70e
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2025-10-06 20:56:16 +0200

    Ejercicio1tema3

---
+++ Tema3/Ejercicio1Tema3/src/ejercicio1tema3/Ejercicio1Tema3.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1tema3;
+import java.util.Scanner;
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio1Tema3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        int numeroPedido;
+        
+        System.out.println("Por favor, introduzca un numero: ");
+        numeroPedido = entrada.nextInt();
+        
+        if (numeroPedido > 0) {
+            System.out.println("El numero introducido es positivo");
+        }else if (numeroPedido < 0){
+            System.out.println("El numero introducido es negativo");
+        }
+        else if (numeroPedido == 0){
+                System.out.println("El numero introducido es 0");
+             }
+        
+    }
+    
+}
