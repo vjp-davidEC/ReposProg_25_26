84c2de5b4e12996a96b448c57054a93ae4313d1b
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2025-10-24 20:51:13 +0200

    Ejericicios Tema4

--- No_Obligatorios/Tema4/Ejercicio15Tema4/src/ejercicio15tema4/Ejercicio15Tema4.java
+++ No_Obligatorios/Tema4/Ejercicio15Tema4/src/ejercicio15tema4/Ejercicio15Tema4.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio15tema4;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -26,9 +27,6 @@ public class Ejercicio15Tema4 {
         System.out.print("Introduce el primer numero: ");
         numero2 = entrada.nextInt();
         
-        numero1 = pedirNumero(entrada);
-        numero2 = pedirNumero(entrada);
-        
         do {
             mostrarMenu();
             opcion = pedirOpcion(entrada);
@@ -38,17 +36,13 @@ public class Ejercicio15Tema4 {
                     System.out.println("Resultado de la suma: " + sumar(numero1, numero2));//SUMA
                     break;
                 case 2:
-                    System.out.println("La resta es: " + (numero1 - numero2));//RESTA
+                    System.out.println("Resultado de la resta: " + restar(numero1, numero2));//RESTA
                     break;
                 case 3:
-                    System.out.println("La multiplicacion es: " + (numero1 * numero2));//MULTIPLICACION
+                    System.out.println("Resultado de la multiplicacion: " + multiplicar(numero1, numero2));//MULTIPLICACION
                     break;
                 case 4:
-                    try {
-                        System.out.println("La division es: " + (numero1 / numero2));//DIVISION
-                    } catch (ArithmeticException e) {
-                        System.out.println("No se puede dividir entre 0");//ERROR
-                    }
+                    dividir(numero1, numero2);//DIVISION
                     break;
                 case 5:
                     System.out.println("Saliendo del programa...");//SALIR
@@ -57,5 +51,43 @@ public class Ejercicio15Tema4 {
                     System.out.println("Opcion no valida, intentalo de nuevo...");//REPETIR
             }
         } while (opcion !=5);//Que solo es hasta el 5
+    }
+    public static int pedirNumero(Scanner entrada, int numero) {
+        System.out.println(numero);
+        return entrada.nextInt();
+    }
+    public static void mostrarMenu() {
+        System.out.println("1.- Sumar los numeros");
+        System.out.println("2.- Restar los numeros");
+        System.out.println("3.- Multiplicar los numeros");
+        System.out.println("4.- Dividir los numeros");
+        System.out.println("5.- Salir del programa");
+        System.out.print("Elige una opcion: ");
+    }
+    public static int pedirOpcion(Scanner entrada) {
+        try {
+            return entrada.nextInt();
+        } catch (InputMismatchException e) {
+            entrada.next();
+            return -1;
+        }
+    }
+    public static int sumar(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+    public static int restar(int numero1, int numero2) {
+        return numero1 - numero2;
+    }
+    public static int multiplicar(int numero1, int numero2) {
+        return numero1 * numero2;
+    }
+    public static int dividir(int numero1, int numero2) {
+        try {
+            int resultado = numero1 / numero2;
+            System.out.println("La division es: " + resultado);//DIVISION
+        } catch (ArithmeticException e) {
+            System.out.println("Error: No se puede dividir entre 0");//ERROR
+        }
+        return 0;
     }
 }
