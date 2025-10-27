e893617273c9ac33ed87d38d7dc17563b2dacb14
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2025-10-27 19:37:35 +0100

    Ejercicios Tema4

--- No_Obligatorios/Tema4/Ejercicio2Tema4/src/ejercicio2tema4/Operaciones.java
+++ No_Obligatorios/Tema4/Ejercicio2Tema4/src/ejercicio2tema4/Operaciones.java
@@ -9,11 +9,11 @@ package ejercicio2tema4;
  * @author DAVID
  */
 public class Operaciones {
-    public static void multiplicar(int numero1, int numero2) {//Metodo de dos numeros enteros que devuelve una suma
+    public static void multiplicar(int numero1, int numero2) {//Metodo de dos numeros enteros que devuelve una multiplicacion
         int resultado = numero1 * numero2;
         System.out.println("La operacion que se realizo es producto y el resultado es " + resultado);
     }
-    public static void sumar(int numero1, int numero2) {//Metodo de dos numeros enteros que devuelve una multiplicacion
+    public static void sumar(int numero1, int numero2) {//Metodo de dos numeros enteros que devuelve una suma
         int resultado = numero1 + numero2;
         System.out.println("La operacion que se realizo es suma y el resultado es " + resultado);
     }
