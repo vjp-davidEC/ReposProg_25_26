573c8f30b80eedc7c2aa0fff66064b5830c70dfe
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2025-10-30 17:17:14 +0100

    Ejercicio 16Tema4

---
+++ No_Obligatorios/Tema4/Ejercicio16Tema4/src/ejercicio16tema4/Ejercicio16Tema4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio16tema4;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio16Tema4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Raiz cuadrada de 64: " + Math.sqrt(64));//Calcular raiz cuadrada de x
+        System.out.println("8 elevado al cubo: " + Math.pow(8, 3));//Calcular elevado a x
+        System.out.println("Exponencial de 2: " + Math.exp(2));//Calcular exponencial a x
+        System.out.println("Logaritmo de 2,71828: " + Math.log(2.71828));//Calcular logaritmo de x
+        System.out.println("Menor valor entre 2 y 3: " + Math.min(2, 3));//Calcular el menor valor entre x e y
+        System.out.println("Valor absoluto de -4,5: " + Math.abs(-4.5));//Calcular el valor absoluto de -x
+        System.out.println("Redondeando -4,5 con ROUND: " + Math.round(-4.5));//Calcular el redondeo de -x
+        System.out.println("Seno de 45 grados: " + Math.sin(45));//Calcular el seno de x
+    }
+}
