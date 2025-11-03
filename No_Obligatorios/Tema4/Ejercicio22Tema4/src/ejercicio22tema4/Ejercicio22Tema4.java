f5ff919821aea818ad02cdb76048475176d6af55
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2025-11-03 20:33:04 +0100

    Ejercicio 22Tema4 Actualizado

--- No_Obligatorios/Tema4/Ejercicio22Tema4/src/ejercicio22tema4/Ejercicio22Tema4.java
+++ No_Obligatorios/Tema4/Ejercicio22Tema4/src/ejercicio22tema4/Ejercicio22Tema4.java
@@ -14,10 +14,7 @@ public class Ejercicio22Tema4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double PI = Math.PI;//Guardar la constante PI con un double
-        double Euler = Math.E;//Guardar la constante Euler con un double
-        
-        mostrarConstantes(PI, Euler);//Llamo al metodo mostrarConstantes pasando el PI y Euler como parametro 
+        mostrarConstantes(Math.PI, Math.E);//Llamo al metodo mostrarConstantes pasando el PI y Euler como parametro 
     }
     public static void mostrarConstantes(double PI, double Euler) {//Metodo que muestra en pantalla los valores de PI y Euler en el mensaje
         System.out.println("Valor de la constante PI: " + PI);
