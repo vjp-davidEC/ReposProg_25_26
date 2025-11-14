2f7115a3c6c8b37c5e9de50505ee5cb6bd9ad271
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2025-11-14 19:26:22 +0100

    Ejercicio 20Tema5

---
+++ No_Obligatorios/Tema5/Ejercicio20Tema5/src/ejercicio20tema5/Cuenta.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio20tema5;
+
+/**
+ *
+ * @author alumno
+ */
+public class Cuenta {
+    //Atributos
+    private float saldo;
+    
+    //Constructores
+    public Cuenta(){
+        this.saldo = 0;
+    }
+    
+    public Cuenta(float s){
+        this.saldo = s;
+    }
+    
+    //Getter y Setter
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+    //Métodos
+    
+}
