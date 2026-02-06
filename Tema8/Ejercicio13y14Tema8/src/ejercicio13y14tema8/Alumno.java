56d08550f7562e380f1a7e6591a3c3e5984d1030
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2026-02-06 16:03:24 +0100

    Ejercicios 6-11y12-13y14

---
+++ Tema8/Ejercicio13y14Tema8/src/ejercicio13y14tema8/Alumno.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio13y14tema8;
+
+/**
+ *
+ * @author alumno
+ */
+public class Alumno {
+    //Atributos
+    private String nombre;
+    private int edad;
+    private float notaMedia;
+    
+    //Constructores
+    public Alumno(){
+        nombre = "";
+        edad = 0;
+        notaMedia = 0f;
+    }
+
+    public Alumno(String nombre, int edad, float notaMedia) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.notaMedia = notaMedia;
+    }
+
+    //Getters/Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getNotaMedia() {
+        return notaMedia;
+    }
+
+    public void setNotaMedia(float notaMedia) {
+        this.notaMedia = notaMedia;
+    }
+
+    //Mostrar
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "\nEdad: " + edad + "\nNota media: " + notaMedia;
+    }
+}
