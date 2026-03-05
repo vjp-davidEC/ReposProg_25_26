feef2d49ed170bb458c842c607e7880d06dabcf7
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2026-03-05 17:43:18 +0100

    Ejercicios Tema10

---
+++ No_Obligatorios/Tema10/Ejercicio15Tema10/src/ejercicio15tema10/Empresa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio15tema10;
+
+/**
+ *
+ * @author alumno
+ */
+public class Empresa {
+    private String nombreEmpresa;
+    private Empleados[] empleados;
+    
+    
+}
