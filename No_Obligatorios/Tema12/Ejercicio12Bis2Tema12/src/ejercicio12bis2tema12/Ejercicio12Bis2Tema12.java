cf05003f72385b50f78e29cb4ef9ca35a3c5cacf
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2026-04-13 20:39:54 +0200

    Ejercicios Tema12

---
+++ No_Obligatorios/Tema12/Ejercicio12Bis2Tema12/src/ejercicio12bis2tema12/Ejercicio12Bis2Tema12.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio12bis2tema12;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio12Bis2Tema12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ No_Obligatorios/Tema12/Ejercicio12BisTema12/src/ejercicio12bistema12/Ejercicio12BisTema12.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio12bistema12;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio12BisTema12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
