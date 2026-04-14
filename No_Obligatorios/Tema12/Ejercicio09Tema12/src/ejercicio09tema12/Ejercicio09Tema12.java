5b7a1b4c7c243c22175eaddce08756abe7d534f0
1DAM - David Escudero Coca <descuderoc03@informatica.iesvalledeljerteplasencia.es>
2026-04-14 17:20:31 +0200

    Ejercicios Tema12

--- No_Obligatorios/Tema12/Ejercicio09Tema12/src/ejercicio09tema12/Ejercicio09Tema12.java
+++ No_Obligatorios/Tema12/Ejercicio09Tema12/src/ejercicio09tema12/Ejercicio09Tema12.java
@@ -4,6 +4,12 @@
  */
 package ejercicio09tema12;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  *
  * @author alumno
@@ -14,7 +20,40 @@ public class Ejercicio09Tema12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        FileWriter fw = null;
+        FileReader fr = null;
+        PrintWriter pw = null;
+
+        try {
+            fr = new FileReader("frase.txt");
+
+            fw = new FileWriter("fraseinvertida.txt");
+            pw = new PrintWriter(fw);
+
+            String frase = "";
+            int codigo = fr.read();
+
+            while (codigo != -1) {
+                char c = (char) codigo;
+                frase += c;
+                codigo = fr.read();
+            }
+
+            String fraseInvertida = "";
+            for (int i = frase.length() - 1; i >= 0; i--) {
+                fraseInvertida += frase.charAt(i);
+            }
+            
+            pw.println(fraseInvertida);
+            System.out.println("Archivo fraseinvertida.txt creado correctamente");
+            
+            pw.close();
+            fw.close();
+            fr.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontro el archivo frase.txt");
+        } catch (IOException e) {
+            System.out.println("Error al leer o escribir en los archivos");
+        }
     }
-    
 }
